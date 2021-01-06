1a290734587ed32bb64bdbc28c89819f3db1e53e
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-06 22:15:23 +0700

    10 pi

--- vstarcam.py
+++ vstarcam.py
@@ -1,11 +1,11 @@
 import cv2
 import urllib.request
 
-cap = cv2.VideoCapture('http://192.168.1.126:14026/videostream.cgi?user=admin&pwd=888888')
-urlup ='http://192.168.1.126:14026/decoder_control.cgi?loginuse=admin&loginpas=888888&command=1&onestep=0&16099090134280.6695055990897747&_=1609909013428'
-urldown ='http://192.168.1.126:14026/decoder_control.cgi?loginuse=admin&loginpas=888888&command=3&onestep=0&16099090304380.6935537129919782&_=1609909030438'
-urlleft ='http://192.168.1.126:14026/decoder_control.cgi?loginuse=admin&loginpas=888888&command=5&onestep=0&16099090589110.1875092007795247&_=1609909058912'
-urlright ='http://192.168.1.126:14026/decoder_control.cgi?loginuse=admin&loginpas=888888&command=7&onestep=0&16099090815000.7435879814536643&_=1609909081500'
+cap = cv2.VideoCapture('http://192.168.1.126:27261/videostream.cgi?user=admin&pwd=888888')
+urlup ='http://192.168.1.126:27261/decoder_control.cgi?loginuse=admin&loginpas=888888&command=0&onestep=1&16099455500550.7579112705690318&_=1609945550056'
+urldown ='http://192.168.1.126:27261/decoder_control.cgi?loginuse=admin&loginpas=888888&command=4&onestep=1&16099455500550.7579112705690318&_=1609945550056'
+urlleft ='http://192.168.1.126:27261/decoder_control.cgi?loginuse=admin&loginpas=888888&command=2&onestep=1&16099455500550.7579112705690318&_=1609945550056'
+urlright ='http://192.168.1.126:27261/decoder_control.cgi?loginuse=admin&loginpas=888888&command=6&onestep=1&16099455500550.7579112705690318&_=1609945550056'
 while True :
     _, frame = cap.read()
     cv2.imshow('frame',frame)

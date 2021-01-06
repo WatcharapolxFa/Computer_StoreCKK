fe3fd1a45dadf623eacd93366d7e924b89b99d03
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-06 12:01:34 +0700

    wasd

--- vstarcam.py
+++ vstarcam.py
@@ -1,11 +1,11 @@
 import cv2
 import urllib.request
 
-cap = cv2.VideoCapture('http://192.168.1.126:31183/videostream.cgi?user=admin&pwd=888888')
-urlup =''
-urldown =''
-urlleft =''
-urlright =''
+cap = cv2.VideoCapture('http://192.168.1.126:14026/videostream.cgi?user=admin&pwd=888888')
+urlup ='http://192.168.1.126:14026/decoder_control.cgi?loginuse=admin&loginpas=888888&command=1&onestep=0&16099090134280.6695055990897747&_=1609909013428'
+urldown ='http://192.168.1.126:14026/decoder_control.cgi?loginuse=admin&loginpas=888888&command=3&onestep=0&16099090304380.6935537129919782&_=1609909030438'
+urlleft ='http://192.168.1.126:14026/decoder_control.cgi?loginuse=admin&loginpas=888888&command=5&onestep=0&16099090589110.1875092007795247&_=1609909058912'
+urlright ='http://192.168.1.126:14026/decoder_control.cgi?loginuse=admin&loginpas=888888&command=7&onestep=0&16099090815000.7435879814536643&_=1609909081500'
 while True :
     _, frame = cap.read()
     cv2.imshow('frame',frame)

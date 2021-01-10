78318d7611d5cc6222fcd6780ca094fc1110b156
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-11 00:54:24 +0700

    full

---
+++ ex_tensor.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array(40)
+print(a.shape)
+b = np.array([40,130])
+print(b.shape)
+c = np.array([[1,2,3],[4,5,6]])
+print(c.shape)
+d = np.array([[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]]])
+print(d.shape)

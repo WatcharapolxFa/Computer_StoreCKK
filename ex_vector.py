2346fc5201d43df3ef2a9b823437354b615324d1
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-11 01:24:34 +0700

    str- 50 pi

---
+++ ex_vector.py
@@ -0,0 +1,14 @@
+import numpy as np
+import time
+
+ a = np.random.rand(2000,2000)
+ c = np.zeros(2000)
+t1 = time.time()
+for j in range(100) :
+  for i in range(a.shape[0]):
+
+    c += a[ i, :]
+
+t2 = time.time()
+print('TIME [ row vector ]:',(t2,t1)/100)
+

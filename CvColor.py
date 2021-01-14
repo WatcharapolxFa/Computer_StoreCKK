9b94f299d98583e056aa23f3ddbe12e97ed78d74
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-14 23:28:53 +0700

    full

---
+++ CvColor.py
@@ -0,0 +1,12 @@
+import cv2
+
+cap = cv2.VideoCapture(0)  # IP
+while True:
+    _, frame = cap.read()
+    R = frame[:, :, 2]
+    G = frame[:, :, 1]
+    B = frame[:, :, 0]
+    mask = (R > 100) & (G < 50) & (B < 50)
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask*1.0)
+    cv2.waitKey(1)

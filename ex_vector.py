c13670d743282f07929b4385db2abe3f92bcdcc8
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-14 22:12:28 +0700

    full

--- ex_vector.py
+++ ex_vector.py
@@ -1,13 +1,17 @@
 import numpy as np
 import time
-a = np.random.rand(2000,2000)
+a = np.random.rand(2000, 2000)
 c = np.zeros(2000)
 t1 = time.time()
-for j in range(100) :
-  for i in range(a.shape[0]):
-    c += a[ i, :]
-
+for j in range(100):
+    for i in range(a.shape[0]):
+        c += a[i, :]
 t2 = time.time()
-print('TIME [ row vector ]:',(t2,t1)/100)
-
-
+print('TIME [ row vector ]:', (t2-t1)/100)
+c = np.zeros(2000)
+t1 = time.time()
+for j in range(100):
+    for i in range(a.shape[1]):
+        c += a[:, i]
+t2 = time.time()
+print('TIME [ columnvector ]:', (t2-t1)/100)

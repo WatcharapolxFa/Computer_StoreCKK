cf3dc74da2e88c7d10fd9374276701632b23d573
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-16 23:20:55 +0700

    full

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -2,4 +2,10 @@ import numpy as np
 
 
 def display(p):
-    p = (p.reshape((3, 3)))
+    print = (p.reshape((3, 3)))
+
+
+def fitness(p):
+    p = p.reshape((3, 3))
+    sum_rows = p.sum(axis=0)
+    sum_cols = p.sum(axis=1)

d00570230f18fe261a1c2652d899ac4ba1468f5f
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-17 11:48:00 +0700

    full

--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,7 @@
 import numpy as np
-X = np.array({[1, 2], [2.5, 3], [3, 1], [-3, -1.7], [-1.6, -3], [1.5, -3]})
+X = np.array([[1, 2], [2.5, 3], [3, 1], [-3, -1.7], [-1.6, -3], [1.5, -3]])
 T = [1, 1, 1, -1, -1, -1]
-W = X[1].copy()
+W = X[4].copy()
 done = False
 while not done:
     done = True

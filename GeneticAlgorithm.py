bfbc2c2366618bcfd6469559ea82785f94b70b89
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-17 12:19:52 +0700

    full

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -14,6 +14,26 @@ def fitness(p):
     return len(np.unique(sum_all))
 
 
-p = np.arange(1, 10)
-display(p)
-print(fitness(p))
+n = 100
+pop = []
+for i in range(n):
+    p = np.arange(1, 10)
+    np.random.shuffle(p)
+    pop.append(p)
+pop = np.array(pop)
+
+while True:
+    fit = []
+    for p in pop:
+        fit.append(fitness(p))
+    fit = np.array(fit)
+    idx = fit.argsort()
+    best = fit[idx[0]]
+    print(best)
+    pop = pop[idx]
+    if best == 1:
+        display(pop[0])
+        break
+    for i in range(1, n):
+        idx = np.random.choice(9, 2)
+        pop[i][idx] = pop[i][idx[::-1]]

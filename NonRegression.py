7c170ae3de44a94eca164acfc9f3fe2cf3e51614
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-21 23:13:24 +0700

    full

--- NonRegression.py
+++ NonRegression.py
@@ -2,10 +2,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 n = 100
 x = np.random.rand(n)
-y = 5*x + 10 + 0.4*np.random.rand(n)
+y = 5*x**2 - 2*x+10 + 0.4*np.random.rand(n)
 plt.plot(x, y, '.')
 
-X = np.vstack((x, np.ones(n))).T
+X = np.vstack((x**2, x, np.ones(n))).T
 W = np.linalg.inv(X.T @ X) @ X.T @ y
 print(W)
 z = X @ W

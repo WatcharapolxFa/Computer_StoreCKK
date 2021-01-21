ddfe26104a9af498037ace661c7738e9083a8eba
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-21 23:12:03 +0700

    full

---
+++ NonRegression.py
@@ -0,0 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt
+n = 100
+x = np.random.rand(n)
+y = 5*x + 10 + 0.4*np.random.rand(n)
+plt.plot(x, y, '.')
+
+X = np.vstack((x, np.ones(n))).T
+W = np.linalg.inv(X.T @ X) @ X.T @ y
+print(W)
+z = X @ W
+plt.plot(X, z, '.r')
+
+plt.show()

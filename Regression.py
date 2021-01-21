75ef3f35bee7af9d6aaac99442b39259e1ac5dda
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-21 23:10:09 +0700

    full

--- Regression.py
+++ Regression.py
@@ -5,10 +5,10 @@ x = np.random.rand(n)
 y = 5*x + 10 + 0.4*np.random.rand(n)
 plt.plot(x, y, '.')
 
-x = np.vstack((x, np.ones(n))).T
-W = np.linalg.inv(X.T@X)@X.T@y
+X = np.vstack((x, np.ones(n))).T
+W = np.linalg.inv(X.T @ X) @ X.T @ y
 print(W)
 z = X @ W
-plt.plot(X, Z, '.r')
+plt.plot(X, z, '.r')
 
 plt.show()
